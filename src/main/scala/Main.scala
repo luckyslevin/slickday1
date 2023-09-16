99a59c4ee80f27457cb1deab3dbf728ab594e3c2
McLOVIN <shortcut7777@gmail.com>
2023-09-16 15:49:52 +0800

    fix: dbio implementation

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -11,37 +11,46 @@ object Main extends App {
   def run(dal: DAL with ProfileComponent) = {
     val db = dal.db
     println("Running test against " + dal.profile)
+
     val dbio = for {
-      _ <- db.run(dal.createTable)
+      _ <- dal.createTable
       val account = Account("Account@gmail.com")
       val profile = Profile(account.id, Some("firstName"), Some("lastName"), '♀')
-      _ <- db.run(dal.createAccount(account))
-      _ <- db.run(dal.createProfile(Profile(account.id, Some("firstName"), Some("lastName"), '♀')))
-      accountOpt <- db.run(dal.findAccount(account.id))
-      profileOpt <- db.run(dal.findProfile(account.id))
-      genders <- db.run(dal.getProfileByGender(profile.gender))
-      a = dal.getAccountProfiles.statements.head
-      accountWithProfiles <- db.run(dal.getAccountProfiles)
-      profileByEmail <- db.run(dal.findProfileByEmail(account.email).result.headOption)
-      accountsByEmail <- db.run(dal.getAccountsByEmail("Acc"))
-      `firstName&LastName` <- db.run(dal.`findFirstName&AndLastName`(account.email))
-    } yield {
-      println("=" * 100)
-      println("Find account: ", accountOpt)
-      println("=" * 100)
-      println("Find profile: ", profileOpt)
-      println("=" * 100)
-      println("Get profile by gender: ", genders)
-      println("=" * 100)
-      println("Get accounts with profiles: ", accountWithProfiles)
-      println("=" * 100)
-      println("Find profile by email: ", profileByEmail)
-      println("=" * 100)
-      println("GET accounts by email: ", accountsByEmail)
-      println("=" * 100)
-      println("Find firstname and lastname by email: ", `firstName&LastName`)
+      _ <- dal.createAccount(account)
+      _ <- dal.createProfile(Profile(account.id, Some("firstName"), Some("lastName"), '♀'))
+      accountOpt <- dal.findAccount(account.id)
+      profileOpt <- dal.findProfile(account.id)
+      genders <- dal.getProfileByGender(profile.gender)
+      accountWithProfiles <- dal.getAccountProfiles
+      profileByEmail <- dal.findProfileByEmail(account.email).result.headOption
+      accountsByEmail <- dal.getAccountsByEmail("Acc")
+      `firstName&LastName` <- dal.`findFirstName&AndLastName`(account.email)
+    } yield (accountOpt, profileOpt, genders, accountWithProfiles, profileByEmail, accountsByEmail, `firstName&LastName`)
+    db.run(dbio).map {
+      case (
+            accountOpt,
+            profileOpt,
+            genders,
+            accountWithProfiles,
+            profileByEmail,
+            accountsByEmail,
+            firstLastName
+          ) =>
+        println("=" * 190)
+        println("Find account: ", accountOpt)
+        println("=" * 190)
+        println("Find profile: ", profileOpt)
+        println("=" * 190)
+        println("Get profile by gender: ", genders)
+        println("=" * 190)
+        println("Get accounts with profiles: ", accountWithProfiles)
+        println("=" * 190)
+        println("Find profile by email: ", profileByEmail)
+        println("=" * 190)
+        println("GET accounts by email: ", accountsByEmail)
+        println("=" * 190)
+        println("Find firstname and lastname by email: ", firstLastName)
     }
-    dbio
   }
 
   run(new DAL with ProfileComponent {
